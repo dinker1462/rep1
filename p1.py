dba84648d47962e1bc0a04b68504f9725a3b4194
Dinker <Dinker@Dinkers-Macbook-Air-2.local>
2016-10-23 18:28:43 +0530

    offline changes

---
+++ p1.py
@@ -0,0 +1,13 @@
+print "Welcome to Python"
+a=2
+b=3
+s=a+b
+d=a-b
+p=a*b
+r=a/b
+m=a%b
+print "The sum is "+ str(s)
+print "The difference is "+ str(d)
+print "The product is "+ str(p)
+print "The quotient is "+ str(r)
+print "The remainder is "+ str(m)
